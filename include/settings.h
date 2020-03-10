4821406fba5f215ee69cddcb0bf403db16d0cb26
Damian Parrino <dparrino@gmail.com>
2020-03-10 10:02:59 -0300

    Version update

--- include/settings.h
+++ include/settings.h
@@ -1,5 +1,5 @@
 
-#define APOLLO_VERSION			"0.9.0"		//Apollo PS3 version (about menu)
+#define APOLLO_VERSION			"0.9.1"		//Apollo PS3 version (about menu)
 
 #define MENU_TITLE_OFF			30			//Offset of menu title text from menu mini icon
 #define MENU_ICON_OFF 			70          //X Offset to start printing menu mini icon
